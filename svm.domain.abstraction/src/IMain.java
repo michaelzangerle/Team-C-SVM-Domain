db2db2107cde6a5af788fb71a4861e604b1cbf50
Michael Zangerle <michael.zangerle@gmail.com>
2012-10-19 17:29:25 +0200

    init without xml files

---
+++ svm.domain.abstraction/src/IMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mike
+ * Date: 19.10.12
+ * Time: 16:56
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IMain {
+}
