2b96035058e9a55af7032d923c9d41679da8d318
thomasgaida <thomas.gaida@students.fhv.at>
2012-10-19 18:14:54 +0200

    test

--- svm.domain.implementation/src/Main.java
+++ svm.domain.implementation/src/Main.java
@@ -1,10 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * User: mike
- * Date: 19.10.12
- * Time: 16:56
- * To change this template use File | Settings | File Templates.
- */
+
 public class Main {
 
     public static void main(String[] args) {
