09154dc57633031bf252112c2c8f64a1556b4cbc
thomasgaida <thomas.gaida@students.fhv.at>
2012-10-30 16:53:07 +0100

    Contact Detail Domänenlogik eingefügt

--- svm.domain.abstraction/src/svm/domain/abstraction/modelInterfaces/IContactDetails.java
+++ svm.domain.abstraction/src/svm/domain/abstraction/modelInterfaces/IContactDetails.java
@@ -1,5 +1,7 @@
 package svm.domain.abstraction.modelInterfaces;
 
+import svm.domain.abstraction.exception.DomainAttributeException;
+
 /**
  * Projectteam: Team C
  * Date: 24.10.12
@@ -7,41 +9,41 @@ package svm.domain.abstraction.modelInterfaces;
 public interface IContactDetails extends IModel {
     String getPhone2();
 
-    void setPhone2(String phone2);
+    void setPhone2(String phone2) throws DomainAttributeException;
 
     String getEmail1();
 
-    void setEmail1(String email1);
+    void setEmail1(String email1) throws DomainAttributeException;
 
     String getEmail2();
 
-    void setEmail2(String email2);
+    void setEmail2(String email2) throws DomainAttributeException;
 
     String getFax();
 
-    void setFax(String fax);
+    void setFax(String fax) throws DomainAttributeException;
 
     String getStreet();
 
-    void setStreet(String street);
+    void setStreet(String street) throws DomainAttributeException;
 
     String getStreetNumber();
 
-    void setStreetNumber(String streetNumber);
+    void setStreetNumber(String streetNumber) throws DomainAttributeException;
 
     ILocation getLocation();
 
-    void setLocation(ILocation location);
+    void setLocation(ILocation location) throws DomainAttributeException;
 
     String getCoordLat();
 
-    void setCoordLat(String coordLat);
+    void setCoordLat(String coordLat) throws DomainAttributeException;
 
     String getCoordLong();
 
-    void setCoordLong(String coordLong);
+    void setCoordLong(String coordLong) throws DomainAttributeException;
 
     String getPhone1();
 
-    void setPhone1(String phone1);
+    void setPhone1(String phone1) throws DomainAttributeException;
 }
