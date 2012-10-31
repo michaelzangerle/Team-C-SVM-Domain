986be6d4f8186c258a1d29d3cb5eb9abf9ffa58d
johannes <johannes@greencode.at>
2012-10-31 11:43:11 +0100

    Added new Libs of Persistence, IMemberDao get Method

--- svm.domain.abstraction/src/svm/domain/abstraction/modeldao/IMemberModelDAO.java
+++ svm.domain.abstraction/src/svm/domain/abstraction/modeldao/IMemberModelDAO.java
@@ -1,10 +1,15 @@
 package svm.domain.abstraction.modeldao;
 
+import svm.domain.abstraction.modelInterfaces.IDepartment;
 import svm.domain.abstraction.modelInterfaces.IMember;
+import svm.persistence.abstraction.exceptions.NoSessionFoundException;
+
+import java.util.List;
 
 /**
  * ProjectTeam: Team C
  * Date: 30.10.12
  */
 public interface IMemberModelDAO extends IModelDAO<IMember> {
+    List<IMember> get(Integer sessionId, String firstName, String lastName, IDepartment department) throws NoSessionFoundException;
 }
--- svm.domain.implementation/src/svm/domain/implementation/modeldao/MemberModelDAO.java
+++ svm.domain.implementation/src/svm/domain/implementation/modeldao/MemberModelDAO.java
@@ -1,10 +1,18 @@
 package svm.domain.implementation.modeldao;
 
+import svm.domain.abstraction.modelInterfaces.IDepartment;
+import svm.domain.abstraction.modelInterfaces.IHasEntity;
 import svm.domain.abstraction.modelInterfaces.IMember;
 import svm.domain.abstraction.modeldao.IMemberModelDAO;
 import svm.domain.implementation.model.Member;
 import svm.persistence.PersistenceFacade;
+import svm.persistence.abstraction.dao.CompareObject;
+import svm.persistence.abstraction.dao.FindQualifiers;
+import svm.persistence.abstraction.exceptions.NoSessionFoundException;
 import svm.persistence.abstraction.model.IMemberEntity;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * ProjectTeam: Team C
@@ -18,5 +26,19 @@ public class MemberModelDAO extends AbstractModelDAO<IMember, IMemberEntity> imp
     @Override
     protected IMember wrapObject(IMemberEntity Entity) {
         return new Member(Entity);
+    }
+
+    @Override
+    public List<IMember> get(Integer sessionId, String firstName, String lastName, IDepartment department) throws NoSessionFoundException {
+        CompareObject[] compares = new CompareObject[3];
+        compares[0] = new CompareObject("firstName", FindQualifiers.LIKE, "%" + firstName + "%");
+        compares[2] = new CompareObject("lastName", FindQualifiers.LIKE, "%" + lastName + "%");
+        compares[2] = new CompareObject("department", FindQualifiers.LIKE, String.valueOf(((IHasEntity) department).getEntity().getId()));
+
+        List<IMember> result = new LinkedList<IMember>();
+        for (IMemberEntity member : getDAO().find(sessionId, compares)) {
+            result.add(wrapObject(member));
+        }
+        return result;
     }
 }
