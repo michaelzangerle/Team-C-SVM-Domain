a186b7cd3b3fded0f3bf219c408c05e068b73f44
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-02 10:53:57 +0100

    IModel Implements Serializeble

--- svm.domain.abstraction/src/svm/domain/abstraction/modelInterfaces/IModel.java
+++ svm.domain.abstraction/src/svm/domain/abstraction/modelInterfaces/IModel.java
@@ -1,8 +1,10 @@
 package svm.domain.abstraction.modelInterfaces;
 
+import java.io.Serializable;
+
 /**
  * ProjectTeam: Team C
  * Date: 25.10.12
  */
-public interface IModel {
+public interface IModel extends Serializable {
 }
