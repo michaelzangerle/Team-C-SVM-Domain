47f876fdefb4d3ed02e9383cf1ed9039cba89621
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-03 12:07:39 +0100

    IModel Remoted

--- svm.domain.abstraction/src/svm/domain/abstraction/modelInterfaces/IModel.java
+++ svm.domain.abstraction/src/svm/domain/abstraction/modelInterfaces/IModel.java
@@ -1,10 +1,11 @@
 package svm.domain.abstraction.modelInterfaces;
 
 import java.io.Serializable;
+import java.rmi.Remote;
 
 /**
  * ProjectTeam: Team C
  * Date: 25.10.12
  */
-public interface IModel extends Serializable {
+public interface IModel extends Serializable,Remote {
 }
