27a92b6220a1d3bc3ee939cd89799474be58c1b8
Michael Zangerle <michael.zangerle@gmail.com>
2012-11-07 14:28:41 +0100

    member fee and libs

--- svm.domain.abstraction/src/Main.java
+++ svm.domain.abstraction/src/Main.java
@@ -2,10 +2,14 @@ import svm.domain.abstraction.DomainFacade;
 import svm.domain.abstraction.exception.DomainException;
 import svm.domain.abstraction.exception.DomainParameterCheckException;
 import svm.domain.abstraction.modelInterfaces.IContest;
+import svm.domain.abstraction.modelInterfaces.IMember;
 import svm.domain.abstraction.modelInterfaces.ITeam;
+import svm.domain.abstraction.modeldao.IMemberModelDAO;
 import svm.persistence.abstraction.exceptions.ExistingTransactionException;
 import svm.persistence.abstraction.exceptions.NoSessionFoundException;
 import svm.persistence.abstraction.exceptions.NoTransactionException;
+
+import java.util.List;
 
 /**
  * ProjectTeam: Team C
@@ -15,22 +19,22 @@ public class Main {
     public static void main(String[] args) throws NoSessionFoundException, IllegalAccessException, InstantiationException, ExistingTransactionException, NoTransactionException, DomainException, DomainParameterCheckException {
         // Generate a session
         Integer sessionId = DomainFacade.generateSessionId();
-//
-//        // Get ModelDAO for Members
-//        IMemberModelDAO dao = DomainFacade.getMemberModelDAO();
-//        // Get a list of all Members
-//        List<IMember> members = dao.get(sessionId, "Patrik","Jost");
-//        // Iterate over all Members
-//        for (IMember entity : members) {
-//            // Get FirstName of Member
-//            String firstName = entity.getFirstName();
-//            // Get LastName of Member
-//            String lastName = entity.getLastName();
-//
-//            System.out.println(firstName + " " + lastName);
-//        }
-//
-//        System.out.println(members.get(0).hasPaidFee(2012));
+
+        // Get ModelDAO for Members
+        IMemberModelDAO dao = DomainFacade.getMemberModelDAO();
+        // Get a list of all Members
+        List<IMember> members = dao.get(sessionId, "","Jost");
+        // Iterate over all Members
+        for (IMember entity : members) {
+            // Get FirstName of Member
+            String firstName = entity.getFirstName();
+            // Get LastName of Member
+            String lastName = entity.getLastName();
+
+            System.out.println(firstName + " " + lastName);
+        }
+
+        System.out.println(members.get(0).hasPaidFee(2012));
 //
 //        IContestModelDAO dao2 = DomainFacade.getContestModelDAO();
 //        startTransaction(sessionId);
@@ -59,24 +63,24 @@ public class Main {
         // Close Session
 
 
-        IContest contest = DomainFacade.getContestModelDAO().getAll(sessionId).get(0);
-
-
-        ITeam team = contest.getTeams().get(0);
-
-        System.out.println("--------------------");
-        for (ITeam t : contest.getTeams()) {
-            System.out.println(t.getName());
-        }
-        //contest.removeInternalTeam(team);
-        DomainFacade.startTransaction(sessionId);
-        DomainFacade.getContestModelDAO().saveOrUpdate(sessionId, contest);
-        DomainFacade.commitTransaction(sessionId);
-
-        System.out.println("--------------------");
-        for (ITeam t : contest.getTeams()) {
-            System.out.println(t.getName());
-        }
+//        IContest contest = DomainFacade.getContestModelDAO().getAll(sessionId).get(0);
+//
+//
+//        ITeam team = contest.getTeams().get(0);
+//
+//        System.out.println("--------------------");
+//        for (ITeam t : contest.getTeams()) {
+//            System.out.println(t.getName());
+//        }
+//        //contest.removeInternalTeam(team);
+//        DomainFacade.startTransaction(sessionId);
+//        DomainFacade.getContestModelDAO().saveOrUpdate(sessionId, contest);
+//        DomainFacade.commitTransaction(sessionId);
+//
+//        System.out.println("--------------------");
+//        for (ITeam t : contest.getTeams()) {
+//            System.out.println(t.getName());
+//        }
 
 
         DomainFacade.closeSession(sessionId);
